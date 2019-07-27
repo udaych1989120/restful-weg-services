a415bc2ec4efffdb4f7e1253151083357e03934b
Uday <Uday@DESKTOP-2DI427R>
2019-07-28 05:00:01 +0530

    Spring Boot Rest example

---
+++ src/main/java/com/uday/rest/webservices/restfulwegservices/RestfulWegServicesApplication.java
@@ -0,0 +1,13 @@
+package com.uday.rest.webservices.restfulwegservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWegServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWegServicesApplication.class, args);
+	}
+
+}
